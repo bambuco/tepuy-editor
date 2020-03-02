b25b93e9363f1c3a7aeebf9665a8929d6698d6c1
DavidRVN <riozv95@gmail.com>
2020-03-02 12:07:13 -0500

    Template-selector

--- src/plugins/gui.default/component.js
+++ src/plugins/gui.default/component.js
@@ -2,39 +2,54 @@ import { App } from '../../js/app';
 
 export class GuiDefault {
 
-    constructor() {
-        App.registerHook('gui_initialize', this.initialize.bind(this));
-        this.menu = [
-            { text: 'Save', icon: 'ui-icon-disk'},
-            { text: 'Zoom In', icon: 'ui-icon-zoomin'}
-        ];
-    }
-
-    initialize() {
-        const template = $.templates("script#gui-default");
-        template.link(App.container, {}); /*{
-            menuItems: this.menu
-        }
-        }, {
-            menuAction: this.menuClick
-        });*/
-        App.ui = {
-            $sidebar: $('#tpe-sidebar'),
-            $content: $('#tpe-content'),
-            load: this.load.bind(this)
-        };
-    }
-
-    menuClick(ev, ui) {
-        console.log('menuClick');
-        if (!ui.item.children("ul").length) {
-          // Leaf menu item
-          alert(ui.item.text());
-        }
-    }
-
-    load(page) {
-        App.invokeHook('gui_templatechooser');
-    }
-
+constructor() {
+    App.registerHook('gui_initialize', this.initialize.bind(this));
+    this.menu = [
+        { text: 'IMG logo'},
+        { text: 'Archivo'},
+        { text: 'Visualizaci&oacuten'},
+        { text: 'Ayuda'}
+    ];
 }
+
+initialize() {
+    const template = $.templates("script#gui-default");
+    /*template.link(App.container, {}); {
+        menuItems: this.menu
+    }
+    }, {
+        menuAction: this.menuClick
+    });
+    App.ui = {
+        $sidebar: $('#tpe-sidebar'),
+        $content: $('#tpe-content'),
+        load: this.load.bind(this)
+    };*/
+
+
+    template.link(App.container,{
+        menuItems: this.menu
+    },{
+        menuAction: this.menuClick
+    });
+
+    App.ui={
+        $sidebar: $('#tpe-sidebar'),
+        $content: $('#tpe-content'),
+        load: this.load.bind(this)
+    };
+}
+
+menuClick(ev, ui) {
+    console.log('menuClick');
+    if (!ui.item.children("ul").length) {
+      // Leaf menu item
+      alert(ui.item.text());
+    }
+}
+
+load(page) {
+    App.invokeHook('gui_templatechooser');
+}
+
+} 
